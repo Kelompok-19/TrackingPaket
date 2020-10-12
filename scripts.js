39c9490929acc26bd3b96879ecf62c26d67fc86d
oktaviana <oktavianairawan@gmail.com>
2020-10-12 22:18:52 +0700

    merged

--- scripts.js
+++ scripts.js
@@ -2,6 +2,10 @@ function showTable(){
     var resi = document.getElementById("no-resi").value;
     //console.log(resi);
     var popup = document.getElementsByClassName("alert");
+<<<<<<< HEAD
+=======
+    
+>>>>>>> gabungan
     if(resi == ""){   
         for(var i=0; i<popup.length; i++){
             popup[i].style.display = "block";
@@ -9,6 +13,7 @@ function showTable(){
         document.getElementById("no-resi").focus();
     }
     else{
+<<<<<<< HEAD
         var showresi = document.getElementById("show-no-resi").innerHTML = resi;
         var tables = document.getElementById("info-table").style.display="block";
         for(var i=0; i<popup.length; i++){
@@ -17,4 +22,28 @@ function showTable(){
         //console.log("print table")
     }
     
+=======
+        var load = document.getElementById("loading")
+        load.style.display="block";
+        for(var i=0; i<popup.length; i++){
+            popup[i].style.display = "none"
+        }
+        data = setTimeout(showData, 3000);
+    }
+    
+}
+
+function showData(){  
+        console.log("showData")
+        var resi = document.getElementById("no-resi").value;
+        var showresi = document.getElementById("show-no-resi");
+        var tables = document.getElementById("info-table");
+
+        showresi.innerHTML = resi;
+        tables.style.display ="block";
+
+        var load = document.getElementById("loading")
+        load.style.display="none";
+
+>>>>>>> gabungan
 }
